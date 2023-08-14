2ef6d5eb332a1c4968ff8c5ae6b48271839210f4
eguniti <nitin.u.gupta@gmail.com>
2023-08-15 02:49:38 +0530

    app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/api/status', function(req, res) {
       return res.status(500).send('error fetching client from pool');
     }
     //client.query('SELECT now() as time', [], function(err, result) {
-     client.query('SELECT NAME as name, ID as id, AGE as age from public.COMPANY where ID =1;', [], function(err, result) {
+     client.query('SELECT NAME as name, ID as id, AGE as age, ADDRESS as address, SALARY as salary from public.COMPANY where ID =1;', [], function(err, result) {
       //call `done()` to release the client back to the pool
       done();
 
@@ -28,11 +28,13 @@ app.get('/api/status', function(req, res) {
       }
 
       return res.json({
-        request_uuid: uuid.v4(),
         //time: result.rows[0].time
         name: result.rows[0].name,
         id: result.rows[0].id,
-        age: result.rows[0].age
+        age: result.rows[0].age,
+        address: result.rows[0].address,
+        salary: result.rows[0].salary
+
       });
     });
   });
